b0c9293f5ca18bb6994139e694bf07157e9eadda
rensawamo <ren0112ngzk@gmail.com>
2024-03-11 12:22:31 +0900

    first commit

---
+++ apps/lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'core/mutations/get_all_tasks.dart';
+import 'widgets/add_task_widget.dart';
+import 'widgets/task_list_view.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tasks"),
+        actions: [
+          const VxDarkModeButton(
+            showSingleIcon: true,
+          ),
+          IconButton(
+            onPressed: () => GetAllTasksMutation(),
+            icon: const Icon(Icons.refresh),
+          )
+        ],
+      ),
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: TaskListView(),
+        ),
+      ),
+      bottomNavigationBar: const AddTaskWidget(),
+    );
+  }
+}
---
+++ apps/test/unit_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:app/core/data/remote_task_repo.dart';
+
+void main() {
+  test('Fetch All Tasks API Test', () async {
+    final response = await RemoteTaskRepo().fetchAllTasks();
+    expect(response.length, 3);
+  });
+}
